4b22130ad30f8640de4c22bf79491de18372f3d5
Sarah Conway <sarahconway@Sarahs-MacBook-Pro.local>
2020-02-28 19:14:09 -0500

    merging

--- admin/scripts/user.php
+++ admin/scripts/user.php
@@ -1,30 +1,31 @@
 <?php 
-
 require '../phpmailer/PHPMailerAutoload.php';
-
-
-
-function createUser($fname, $username, $password, $email){
+function createUser($fname, $username, $email){
     $pdo = Database::getInstance()->getConnection();
     
     //TODO: finish the below so that it can run a SQL query
     // to create a new user with provided data
     $create_user_query = 'INSERT INTO tbl_user(user_fname, user_name, user_pass, user_email, user_ip)';
     $create_user_query .= ' VALUES(:fname, :username, :password, :email, "no" )';
+    
+    $string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+    $randPassword = randString($string, 8);
 
     $create_user_set = $pdo->prepare($create_user_query);
     $create_user_result = $create_user_set->execute(
         array(
             ':fname'=>$fname,
             ':username'=>$username,
-            ':password'=>$password,
+            ':password'=>$randPassword,
             ':email'=>$email,
+
+         
         )
     );
      //TODO: redirect to index.php if creat user successfully
     // otherwise, return a error message
     if($create_user_result){
-
+    
 
         $mail = new PHPMailer;
 
@@ -54,8 +55,9 @@ function createUser($fname, $username, $password, $email){
             echo 'great success';
         }
 
-
         redirect_to('index.php');
+
+
     }else{
         return 'The user did not go through';
     }
